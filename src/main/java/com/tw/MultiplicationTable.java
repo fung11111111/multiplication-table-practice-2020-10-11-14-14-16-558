98456f11aab1a597ed03ed9169d82a4d08d0a851
Ray Lam <ray.lam@oocl.com>
2020-11-24 16:10:27 +0800

    Update MultiplicationTable.java
    add multiplication table

--- src/main/java/com/tw/MultiplicationTable.java
+++ src/main/java/com/tw/MultiplicationTable.java
@@ -2,6 +2,76 @@ package com.tw;
 
 public class MultiplicationTable {
     public String create(int startNumber, int endNumber) {
-        return null;
+        boolean isValid = isValid(startNumber, endNumber);
+        String multiplicationTable = "";
+        if (isValid) {
+            multiplicationTable = generateMultiplicationTable(startNumber, endNumber);
+            return multiplicationTable;
+
+        }else {
+            multiplicationTable = null;
+        }
+        return multiplicationTable;
+    }
+
+    public String generateMultiplicationTable(int startNumber, int endNumber) {
+        String strTable = "";
+        for(int i=startNumber; i<= endNumber; i++ ){
+            String line = generateMultiplicationLine(startNumber, i);
+            if(i == endNumber){
+                strTable += line;
+            }else{
+                strTable += line + "\r\n";
+            }
+        }
+
+        return strTable;
+    }
+
+    String generateMultiplicationLine(int startNumber, int row) {
+        String strLine = "";
+        for(int i= startNumber; i<= row; i++){
+            if(i == row){
+                strLine += i + "*" + row + "=" + i*row;
+            }else{
+                strLine += i + "*" + row + "=" + i*row + "  ";
+            }
+        }
+
+        return strLine;
+    }
+
+
+    public boolean isValid(int startNumber, int endNumber) {
+        boolean isStartNumInRange = isNUmInRange(startNumber);
+        boolean isEndNumInRange = isNUmInRange(endNumber);
+        boolean isStartSmaller = isEndlarger(startNumber, endNumber);
+        if (isStartNumInRange && isEndNumInRange && isStartSmaller){
+            return true;
+        }else {
+            return false;
+        }
+
+    }
+
+    public boolean isEndlarger(int startNumber, int endNumber) {
+        if(startNumber <= endNumber){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public boolean isNUmInRange(int num) {
+        if(num > 0 && num <= 1000){
+            return true;
+        }else{
+            return  false;
+        }
     }
 }
+
+
+
+
+
